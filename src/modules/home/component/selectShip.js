ab102e228eb0267d2617982053286972243a14a8
stonel9687 <douglas.reyesquintero09@gmail.com>
2021-08-12 23:15:35 -0400

    feat fetch call

--- src/modules/home/component/selectShip.js
+++ src/modules/home/component/selectShip.js
@@ -1,5 +1,5 @@
 function SelectShip(props) {
-  const { handleSelect, selectOn } = props
+  const { handleSelect, selectOn, selectName, shipList, shipDetails, shipDetail } = props
   return (
     <>
       <div className='container'>
@@ -7,17 +7,66 @@ function SelectShip(props) {
           <div className='selectBox'>
             <div className='select d-flex justify-content-between' id='select' onClick={handleSelect}>
               <div className='content-select '>
-                <h1>Seleccion de Naves</h1>
+                <h1>{selectName}</h1>
               </div>
               <div>
                 <i class='fas fa-chevron-down'></i>
               </div>
             </div>
             {selectOn && (
-              <div>
-                <p>executor</p>
-              </div>
+              <>
+                <div>
+                  <p>executor</p>
+                </div>
+                <div>
+                  <p>Death Star</p>
+                </div>
+                <div>
+                  <p>Y-Wing</p>
+                </div>
+              </>
             )}
+
+            <div>
+              {shipList.map((s) => {
+                return (
+                  <p
+                    onClick={() => {
+                      shipDetails(s)
+                    }}
+                  >
+                    {s.name}
+                  </p>
+                )
+              })}
+            </div>
+            <div className='bg-primary'>
+              <div className='description'>
+                <h1>{shipDetail?.name}</h1>
+                <span>{shipDetail?.model}</span>
+                <hr />
+                <h3>Fabricante</h3>
+                <span> {shipDetail?.manufacturer}</span>
+                <h3>largo</h3>
+                <span>{shipDetail?.length}</span>
+                <h3>valor</h3>
+                <span>{shipDetail?.cost_in_credits} creditos</span>
+                <h3>cantidad de pasajeros</h3>
+                <span>{shipDetail?.passengers}</span>
+                <hr />
+              </div>
+              <div className='passanger'>
+                <h1>Pasajeros</h1>
+                <hr />
+                <span> chewacca</span>
+                <br />
+                <span> han solo</span>
+                <br />
+                <span> lando</span>
+                <br />
+                <span> nien</span>
+              </div>
+            </div>
           </div>
         </form>
       </div>
--- src/modules/home/mainView.js
+++ src/modules/home/mainView.js
@@ -1,15 +1,61 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import SelectShip from './component/selectShip'
 
 const Home = () => {
   const [selectOn, setSelectOn] = useState(true)
+  const [selectName, setSelectName] = useState('Seleccion de Naves')
+  const [shipDetail, setShipDetail] = useState([])
+  const [shipList, setShipList] = useState([])
   const handleSelect = () => {
     let seleccion = selectOn ? false : true
     setSelectOn(seleccion)
   }
+
+  const shipDetails = (ship) => {
+    fetch(`${ship.url}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((resp) => {
+        return resp.json()
+      })
+      .then((data) => {
+        console.log(data)
+        setShipDetail(data)
+      })
+      .catch((error) => console.log('Error:', error))
+  }
+
+  const showShip = () => {
+    fetch('https://swapi.dev/api/starships/', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((resp) => {
+        return resp.json()
+      })
+      .then((data) => {
+        setShipList(data.results)
+      })
+      .catch((error) => console.log('Error:', error))
+  }
+
+  useEffect(() => {
+    showShip()
+  }, [])
+
   return (
     <div>
-      <SelectShip handleSelect={handleSelect} selectOn={selectOn} />
+      <SelectShip
+        handleSelect={handleSelect}
+        selectOn={selectOn}
+        selectName={selectName}
+        shipList={shipList}
+        shipDetails={shipDetails}
+        shipDetail={shipDetail}
+      />
     </div>
   )
 }
