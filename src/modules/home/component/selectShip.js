c41ec7177eea9ebb62974aaeb42ea523238e6acb
stonel9687 <douglas.reyesquintero09@gmail.com>
2021-08-18 20:51:13 -0400

    feat desing background img

--- src/modules/home/component/selectShip.js
+++ src/modules/home/component/selectShip.js
@@ -2,7 +2,7 @@ function SelectShip(props) {
   const { handleSelect, selectOn, selectName, shipList, shipDetails, shipDetail, showDetails, showPilots } = props
   return (
     <>
-      <div className='row mb-5'>
+      <div className='row mb-5 box-img'>
         <form className='col-lg-6 col-md-6 col-sm-12 col-12  mx-auto'>
           <div className='selectBox col-lg-6 col-md-6 col-sm-12 col-12  mx-auto'>
             <div className='select d-flex justify-content-between' id='select' onClick={handleSelect}>
@@ -38,18 +38,18 @@ function SelectShip(props) {
                   <h2>{shipDetail?.name}</h2>
                   <span>{shipDetail?.model}</span>
                   <hr />
-                  <h3>Fabricante</h3>
+                  <h4>Fabricante</h4>
                   <span> {shipDetail?.manufacturer}</span>
                   <h3>largo</h3>
                   <span>{shipDetail?.length}</span>
-                  <h3>valor</h3>
+                  <h4>valor</h4>
                   <span>{shipDetail?.cost_in_credits} creditos</span>
-                  <h3>cantidad de pasajeros</h3>
+                  <h4>cantidad de pasajeros</h4>
                   <span>{shipDetail?.passengers}</span>
                   <hr />
                 </div>
                 <div className='description'>
-                  <h1>Pasajeros</h1>
+                  <h2>Pasajeros</h2>
                   <hr />
                   <span> chewacca</span>
                   <br />
